1544658561c2bfb952777e1a85863f915f6687d2
MaorShani <33581053+MaorShani@users.noreply.github.com>
2020-12-11 14:01:21 +0200

    Update blackFriday-finalProject.py
    Fixing a bug with creating dummy variables
    
    Spliting the data to train and test

--- blackFriday-finalProject.py
+++ blackFriday-finalProject.py
@@ -7,11 +7,13 @@ Created on Sun Dec  6 00:26:59 2020
 
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 #import matplotlib as pyplot
 from sklearn import datasets, linear_model
+from sklearn.model_selection import train_test_split
 
-# Read Train DataSet
+# Read DataSet
 
 data=pd.read_csv('C:/Users/maors/Documents/Maor/Data Science/Final Project/Data Sets/train.csv')
 
@@ -25,6 +27,10 @@ data.head()
 data.describe()
 data['User_ID'].nunique()
 data['Product_ID'].nunique()
+
+# data Exploration
+sns.countplot(x ='Gender', data = data)
+plt.show()
 
 # Remove unnecesary columns
 
@@ -41,20 +47,25 @@ pro_cat=pd.concat([cat1,cat2,cat3]).groupby(level=0).any().astype(int)
 ### Some temporary data exploration
 #pro_cat.head(30)
 #pro_cat_head= pro_cat.head(10)
-#data_head =data.head(30)
+data_head =data.head(30)
 #data_describe = data.describe()
 
-# Split the train data set to X and Y. X currently include only the user features
+# Split the data set to X and Y. X currently include only the user features
 
-data_train_x = data[['Gender','Age','Occupation','City_Category','Stay_In_Current_City_Years','Marital_Status']]
-data_train_y = data[['Purchase']]
+data_x = data[['Gender','Age','Occupation','City_Category','Stay_In_Current_City_Years','Marital_Status']]
+data_x = data_x.astype('category')
+data_y = data[['Purchase']]
 
 # Set ALL of the user features as dummy variables.
-data_train_x = pd.get_dummies(data=data_train_x, drop_first=True)
-data_train_x = pd.concat([data_train_x,pro_cat], axis=1)
+data_x = pd.get_dummies(data=data_x, drop_first=True)
+data_x = pd.concat([data_x,pro_cat], axis=1)
+
+# Split the data to train and test
+
+data_train_x, data_test_x, data_train_y, data_test_y = train_test_split(data_x, data_y, test_size=0.33, random_state=1)
 
 ### Temp variable
-#data_train_x_head = data_train_x.head(10)
+data_train_x_head = data_train_x.head(10)
 
 regr = linear_model.LinearRegression()
 regr.fit(data_train_x, data_train_y)
