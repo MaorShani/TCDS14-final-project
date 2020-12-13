52e1e1fc9763d7bd46c3e655175486a9cdf029bc
MaorShani <33581053+MaorShani@users.noreply.github.com>
2020-12-13 11:50:07 +0200

    Update blackFriday-finalProject.py
    Adding graps for data exploration
    
    Exploring Linear Regression
    
    Some Random Forest Wxploration

--- blackFriday-finalProject.py
+++ blackFriday-finalProject.py
@@ -9,10 +9,13 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 #import matplotlib as pyplot
 from sklearn import datasets, linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import tree
 
 # Read DataSet
 
@@ -30,8 +33,24 @@ data['User_ID'].nunique()
 data['Product_ID'].nunique()
 
 # data Exploration
-sns.countplot(x ='Gender', data = data)
+fig, axes = plt.subplots(2, 3, figsize=(18, 10))
+fig.suptitle('Count rows - grouped by features\' values')
+sns.countplot(ax=axes[0,0],x ='Gender', data = data)
+sns.countplot(ax=axes[0,1],x ='Age', data = data, order = ["0-17", "18-25","26-35","36-45","46-50","51-55","55+"])
+sns.countplot(ax=axes[0,2],x ='Marital_Status', data = data)
+sns.countplot(ax=axes[1,0],x ='Occupation', data = data)
+sns.countplot(ax=axes[1,1],x ='City_Category', data = data, order=["A","B","C"])
+sns.countplot(ax=axes[1,2],x ='Stay_In_Current_City_Years', data = data, order = ["0","1","2","3","4+"])
+
 plt.show()
+
+plt.hist(data['Purchase'])
+plt.xlabel("Purchase")
+plt.ylabel("Count")
+plt.title("Purchace Histogram")
+
+plt.show()
+
 
 # Remove unnecesary columns
 
@@ -77,3 +96,58 @@ print(f'R2 is {regr.score(data_train_x, data_train_y)}')
 prediction = regr.predict(data_test_x)
 mse =  mean_squared_error(data_test_y, prediction)
 print(f'MSE is {mse}')
+
+# Random Forest
+
+data_train_y_np = np.array(data_train_y)
+data_train_x_np = np.array(data_train_x)
+
+rf = RandomForestRegressor(max_depth = 5)
+rf.fit(data_train_x_np, data_train_y_np)
+prediction_rf = rf.predict(data_test_x)
+mse_rf =  mean_squared_error(data_test_y, prediction_rf)
+print(f'Random_Forest MSE is {mse_rf}')
+
+fn=data_train_x.columns.values.tolist()
+cn=data_train_y.columns.values.tolist()
+fig, axes = plt.subplots(nrows = 1,ncols = 1,figsize = (4,4), dpi=800)
+tree.plot_tree(rf.estimators_[5],
+               feature_names = fn, 
+               class_names=cn,
+               filled = True);
+#fig.savefig('C:/Users/maors/Documents/Maor/Data Science/Final Project/rf_individualtree.png')
+
+importances = list(rf.feature_importances_)
+feature_importances = [(data_train_x, round(importance, 4)) for data_train_x, importance in zip(fn, importances)]
+feature_importances = sorted(feature_importances, key = lambda x: x[1], reverse = True)
+[print('Variable: {:20} Importance: {}'.format(*pair)) for pair in feature_importances];
+
+# Linear Regression using statmodel in order to get p-values
+
+data_train_x2 = sm.add_constant(data_train_x)
+model = sm.OLS(data_train_y,data_train_x2)
+model2 = model.fit()
+print(model2.summary())
+
+# Removing unimportant columnns based on coefficient p-vlues
+
+data_train_x_reduced = data_train_x.drop(columns=['Gender_M',
+                                                  'Stay_In_Current_City_Years_1',
+                                                  'Stay_In_Current_City_Years_2',
+                                                  'Stay_In_Current_City_Years_3',
+                                                  'Stay_In_Current_City_Years_4+'])
+
+data_test_x_reduced = data_test_x.drop(columns=['Gender_M',
+                                                  'Stay_In_Current_City_Years_1',
+                                                  'Stay_In_Current_City_Years_2',
+                                                  'Stay_In_Current_City_Years_3',
+                                                  'Stay_In_Current_City_Years_4+'])
+
+regr = linear_model.LinearRegression()
+regr.fit(data_train_x_reduced, data_train_y)
+#print(regr.coef_)
+print(f'New R2 is {regr.score(data_train_x_reduced , data_train_y)}')
+prediction = regr.predict(data_test_x_reduced)
+mse =  mean_squared_error(data_test_y, prediction)
+print(f'new MSE is {mse}')
+
